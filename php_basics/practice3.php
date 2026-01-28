2272d60b0c5599c29d4b8eba0dc27357efbc61be
SV3105 <snehavaghela3105@gmail.com>
2026-01-28 13:29:53 +0530

    PHP-006:learn array built-in functions

--- php_basics/practice3.php
+++ php_basics/practice3.php
@@ -13,4 +13,56 @@ function calculateTotal(float $price, int $qty): float
 $total = calculateTotal(500, 3);
 echo $total;
 echo "<br />";
+/*
+function myFunction() {
+  echo "I come from a function!";
+}
+
+$myArr = array("Volvo", 15, "myFunction");
+
+$myArr[2]();
+*/
 ?>
+
+<?php 
+//task- PHP-006(1)
+
+$str = "         Hello World  ";
+echo strtolower(trim($str)). "<br />";
+// u use trim or not , php will always print string with removing all unnecessary spaces
+echo str_replace("World", "PHP", $str). "<br />";
+
+?>
+
+<?php 
+//task- PHP-006(2)
+$numbers = [1, 6, 5, 3, 7, 8, 0];
+echo in_array(5, $numbers). "<br />";
+
+print_r($numbers);
+echo "<br />";
+array_push($numbers, 9);
+print_r($numbers);
+echo "<br />";
+
+$name = ["xyz", "abc"];
+$merged = array_merge($name, $numbers);
+print_r($merged);
+
+/*
+$fname=array("Peter","Ben","Joe");
+$age=array("35","37","43");
+
+$c=array_combine($fname,$age);
+print_r($c); => Array ( [Peter] => 35 [Ben] => 37 [Joe] => 43 )
+*/
+
+// sort() - sorts an indexed array in ascending order
+// rsort() - sorts an indexed array in descending order
+// asort() - sorts an associative array in ascending order, according to the value
+// ksort() - sorts an associative array in ascending order, according to the key
+// arsort() - sorts an associative array in descending order, according to the value
+// krsort() - sorts an associative array in descending order, according to the key
+?>
+
+
