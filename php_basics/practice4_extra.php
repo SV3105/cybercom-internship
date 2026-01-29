541b24f947d6338853227a137869af236a75b307
SV3105 <snehavaghela3105@gmail.com>
2026-01-29 15:36:45 +0530

    implement inheritance

--- php_basics/practice4_extra.php
+++ php_basics/practice4_extra.php
@@ -95,5 +95,32 @@ class counter{
  counter::increment();
  counter::increment();
  counter::increment();
- echo "counter: ". counter::getCount();
+ echo "counter: ". counter::getCount(). "<br>";
+ echo "<br>";
+?>
+
+<?php 
+class person{
+    public $name;
+
+    function __construct($name){
+        $this->name = $name;
+    }
+}
+
+class teacher extends person{
+    public $subject;
+     
+    function __construct($name, $subject){
+        parent::__construct($name);
+        $this->subject = $subject;
+    }
+
+    function getDetails(){
+        echo "Name: ". $this->name . " Subject: ". $this->subject;
+    }
+}
+
+$t1 = new teacher("abc", "Hindi");
+echo $t1->getDetails();
 ?>
