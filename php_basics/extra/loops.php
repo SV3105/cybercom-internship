78ee7dede8d3468790b113a51bbc402e91e8ce85
SV3105 <snehavaghela3105@gmail.com>
2026-02-01 15:52:59 +0530

    numbered diamond pattern implemented

--- php_basics/extra/loops.php
+++ php_basics/extra/loops.php
@@ -25,6 +25,36 @@ function diamond($n){
      echo "</pre>";
 }
 
-
- 
+function numberedDiamond(){
+$rows = 7;
+$count = 1;
+$mid = ceil($rows/2);
+echo "<pre>";
+for($i=1; $i<=$mid; $i++){
+    
+    for($j=($mid-$i); $j>0; $j--){
+        echo "   ";
+    }
+    for($k=1;$k<=($i * 2) - 1; $k++){
+        
+        printf("%3d", $count);
+        $count++;
+    }
+    echo "<br>";
+    
+}
+for($i=$mid-1; $i>0; $i--){
+  
+    for($j=1; $j<=$mid-$i; $j++){
+        echo "   ";
+    }
+    for($k=(2*$i)-1; $k>0; $k--){
+       
+        printf("%3d", $count);
+        $count++;
+    }
+    echo "<br>";
+}
+ echo "</pre>";
+}
 ?>
