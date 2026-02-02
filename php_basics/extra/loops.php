48307a9422eacedfaa5cdd5f05d9f0db64cc1657
SV3105 <snehavaghela3105@gmail.com>
2026-02-02 11:49:32 +0530

    implemented pascal triangle

--- php_basics/extra/loops.php
+++ php_basics/extra/loops.php
@@ -87,8 +87,38 @@ for($i=$mid; $i>0; $i--){
 echo "</pre>";
 }
 
+function pascalTriangle($rows){
+
+echo "<pre>";
+
+$triangle = [];
+
+for($i=0; $i<=$rows; $i++){
+    for($j=($rows-$i); $j>0; $j--){
+        echo " ";
+    }
+    for($k=0; $k<=$i; $k++){
+        if($k == 0 || $k == $i){
+            $sum = 1;
+        }
+        else{
+            $sum = $triangle[$i-1][$k-1] + $triangle[$i-1][$k];
+        }
+
+        $triangle[$i][$k] = $sum;
+        echo $sum . " ";
+    }
+    echo "<br>";
+}
+
+echo "</pre>";
+}
+
+
 // diamond(7);
 // numberedDiamond(9);
 // butterflyPattern(10);
+// pascalTriangle(4);
+
 ?>
 
